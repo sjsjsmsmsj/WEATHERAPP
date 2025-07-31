3c94bf19d191ec6791be5d33f8a7c5ba05d2f037
sjsjsmsmsj <lamgiathinh05@gmail.com>
2025-07-31 16:55:51 +0700

    complete weather app

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -7,50 +7,82 @@ import drizzle_icon from '../assets/drizzle.png'
 import humidity_icon from '../assets/humidity.png'
 import snow_icon from '../assets/snow.png'
 import wind_icon from '../assets/wind.png'
-import { useEffect } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 const Weather = () => {
+    const inputRef = useRef();
+    const [weatherData, setWeatherData] = useState(false);
+    const allIcons = {
+        "01d": clear_icon,
+        "01n": clear_icon,
+        "02d": cloud_icon,
+        "02n": cloud_icon,
+        "03d": cloud_icon,
+        "03n": cloud_icon,
+        "04d": cloud_icon,
+        "04n": cloud_icon,
+        "09d": drizzle_icon,
+        "09n": drizzle_icon,
+        "10d": drizzle_icon,
+        "10n": drizzle_icon,
+        "11d": drizzle_icon,
+        "11n": drizzle_icon,
+        "13d": snow_icon,
+        "13n": snow_icon,
+        "50d": wind_icon
+    }
 
     const search = async (city) => {
         const API_KEY = import.meta.env.VITE_APP_ID;
 
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}&units=metric`;
 
         try {
             const res = await fetch(url);
             const data = await res.json();
             console.log(data);
+            const icon = allIcons[data.weather[0].icon] || clear_icon;
+
+            setWeatherData({
+                humidity: data.main.humidity,
+                windSpeed: data.wind.speed,
+                temperature: Math.floor(data.main.temp),
+                location: data.name,
+                icon: icon
+            });
         } catch (err) {
             console.error(err);
         }
     };
 
     useEffect(() => {
-        search("London");
+        search("Ho Chi Minh");
     }, []);
 
     return (
         <div className='weather'>
             <div className="search-bar">
-                <input type="text" placeholder='Search' />
-                <img src={search_icon} alt="" />
+                <input type="text" placeholder='Search' ref={inputRef} />
+                <img src={search_icon} alt="" onClick={() => {
+                    search(inputRef.current.value);
+                }} />
             </div>
 
             <img src={clear_icon} alt="" className='weather-icon' />
-            <p className='temperature'>16°C</p>
-            <p className='location'>Việt Nam</p>
+            <p className='temperature'>{weatherData.temperature}°C</p>
+            <p className='location'>{weatherData.location}</p>
             <div className="weather-data">
                 <div className="col">
                     <img src={humidity_icon} alt="" />
                     <div>
-                        <p>91%</p>
+                        <p>{weatherData.humidity}%</p>
                         <span>Humidity</span>
                     </div>
                 </div>
                 <div className="col">
                     <img src={wind_icon} alt="" />
                     <div>
-                        <p>3.6 km/h</p>
+                        <p>{weatherData.windSpeed} km/h</p>
                         <span>Wind</span>
                     </div>
                 </div>
